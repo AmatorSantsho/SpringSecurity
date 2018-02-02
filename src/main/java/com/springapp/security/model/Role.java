caaa1090b630970022cbbace44d2b76668f0ecf2
AmatorSantsho <ash002@tut.by>
2018-02-02 17:22:08 +0200

    UserDetailsService_Auth

--- src/main/java/com/springapp/security/model/Role.java
+++ src/main/java/com/springapp/security/model/Role.java
@@ -1,9 +1,16 @@
 package com.springapp.security.model;
 
+import org.springframework.security.core.GrantedAuthority;
+
 /**
  * Created by 123 on 21.01.2018.
  */
-public enum Role {
+public enum Role implements GrantedAuthority {
     ROLE_USER,
-    ROLE_ADMIN
+    ROLE_ADMIN;
+
+    @Override
+    public String getAuthority() {
+        return name();
     }
+}
